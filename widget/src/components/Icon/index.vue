78f5462c873edcfb8bbed762f00c8c71cf5819b5
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-11-30 19:59:31 -0300

    feat(widget): icon component

---
+++ widget/src/components/Icon/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="name" v-bind="$props" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import Loading from './Loading'
+import Copy from './Copy'
+import Chat from './Chat'
+import ChevronDown from './ChevronDown'
+
+export default defineComponent({
+  components: {
+    Loading,
+    Copy,
+    Chat,
+    ChevronDown
+  },
+  props: {
+    name: {
+      type: String, required: true
+    }
+  }
+})
+</script>
